ba423064dc00134ff9ae5be01967a4f9c297fc52
Dwesh163 <ehouarn.duriaux@protonmail.com>
2023-11-06 16:17:04 +0100

    [fix] fetch to github

--- main/script.js
+++ main/script.js
@@ -1,21 +1,32 @@
-async function fetchAPI() {
-    return await fetch(
-    `https://raw.githubusercontent.com/dwesh163/tableau-de-notes-Off/main/data.json`, {
-      method: "GET",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Request-Method": "POST, GET, OPTIONS, DELETE"
+async function fetchAPI(url) {
+	try {
+		const response = await fetch(url);
 
-      },
-    }
-  );
+		if (response.ok) {
+			const data = await response.json();
+			return data;
+		} else {
+			console.error('Network response was not ok.');
+		}
+	} catch (error) {
+		console.error('An error occurred:', error);
+	}
 }
 
 const init = async () => {
-  let res = await fetchAPI()
-  console.log(res)
-}
-init()
+	localData = JSON.parse(localStorage.getItem('data'));
 
+	for (var person in localData) {
+		if (localData[person]['type'] == 'github') {
+			data = await fetchAPI(localData[person]["url"]);
+
+			for (var person in data) {
+				localData[person] = data[person];
+			}
+		}
+	}
+
+	localStorage.setItem('data', JSON.stringify(localData));
+};
+
+init();
